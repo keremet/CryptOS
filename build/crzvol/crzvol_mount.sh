b79f9d979deb714f28323161cc6632345054d144
Andrey Sokolov <keremet@solaris.kirov.ru>
2015-09-24 16:17:23 +0400

    Добавлены скрипты для работы с zfs на шифрованных zvol

---
+++ build/crzvol/crzvol_mount.sh
@@ -0,0 +1,1 @@
+sudo lofiadm -c aes-256-cbc -a /dev/zvol/rdsk/rpool/d /dev/lofi/1 && sudo zpool import -d /dev/lofi data
---
+++ build/crzvol/crzvol_unmount.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo zpool export data 
+sudo lofiadm -d /dev/lofi/1
