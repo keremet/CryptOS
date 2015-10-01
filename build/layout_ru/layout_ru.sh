afafbcbb0ba886200e1d4352b69155bd7c4057f2
Andrey Sokolov <keremet@solaris.kirov.ru>
2015-10-01 17:05:19 +0400

    Сборка XMMS выполнена старым компилятором. Добавлена настройка переключения раскладки

---
+++ build/layout_ru/layout_ru.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+exec /usr/bin/setxkbmap -option "grp:ctrl_shift_toggle,grp_led:scroll" -layout "us,ru"
--- build/xmms/build.sh
+++ build/xmms/build.sh
@@ -2,16 +2,14 @@
 
 . ../../lib/functions.sh
 
-pkg install cryptos/gcc pkg:/library/glib1 pkg:/library/desktop/gtk1 pkg:/system/header/header-audio
+pkg install pkg:/developer/gcc-3 pkg:/system/library/math/header-math pkg:/library/glib1 pkg:/library/desktop/gtk1 pkg:/system/header/header-audio
 
 tar xf ../../source/xmms-1.2.11.tar.bz2
 cd xmms-1.2.11
 sed -i -e '33414 s/*)$/* | *-*-solaris*)/g' configure
 chmod a+x configure
 
-PATH=/usr/local/bin:$PATH
-
-CFLAGS="-O3" LDFLAGS="-R/usr/local/lib" ./configure --prefix=/usr/local/xmms-1.2.11
+CFLAGS="-O3" ./configure --prefix=/usr/local/xmms-1.2.11
 make_all
 make install
 
