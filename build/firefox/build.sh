fc831451c804041583460d9827c82f41dc0fc4b6
Andrey Sokolov <keremet@solaris.kirov.ru>
2015-10-05 13:22:06 +0400

    Доделан firefox31

--- build/firefox/build.sh
+++ build/firefox/build.sh
@@ -4,8 +4,7 @@
 
 bunzip2 < ../../source/firefox-31.0.en-US.opensolaris-i386-pkg.bz2 > firefox-31.0.en-US.opensolaris-i386-pkg
 mkdir -p root/var/sadm/install
-pkgadd -R $PWD/root/ -d firefox-31.0.en-US.opensolaris-i386-pkg
+pkgadd -R $PWD/root/ -d firefox-31.0.en-US.opensolaris-i386-pkg all
 rm -r root/var/
-opt/share перенесена в usr. Изменено содержимое файла firefox.desktop. Изменены имена обоих файлов.
 
 publish_pkg
