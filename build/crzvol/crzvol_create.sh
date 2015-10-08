be1cb2576b843ac201c8545b44f46de466538788
Andrey Sokolov <keremet@solaris.kirov.ru>
2015-10-08 16:38:25 +0400

    Исправлено создание шифрованного zvol

--- build/crzvol/crzvol_create.sh
+++ build/crzvol/crzvol_create.sh
@@ -8,5 +8,6 @@ fi
 sudo zfs create -V $1 rpool/d && \
 sudo lofiadm -c aes-256-cbc -a /dev/zvol/rdsk/rpool/d /dev/lofi/1 && \
 sudo zpool create data /dev/lofi/1 && \
+sudo chmod -R a+w /data && \
 sudo zfs create data/home
 
