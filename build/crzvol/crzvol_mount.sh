50fed2a96e50503750dd648b70bed9dab4965130
Andrey Sokolov <keremet@solaris.kirov.ru>
2015-10-25 12:36:16 +0400

    Добавлены пункты меню для crzvol

--- build/crzvol/crzvol_mount.sh
+++ build/crzvol/crzvol_mount.sh
@@ -1,1 +1,3 @@
-sudo lofiadm -c aes-256-cbc -a /dev/zvol/rdsk/rpool/d /dev/lofi/1 && sudo zpool import -d /dev/lofi data
+sudo lofiadm -c aes-256-cbc -a /dev/zvol/rdsk/rpool/d /dev/lofi/1 && \
+sudo zpool import -d /dev/lofi data && \
+gnome-commander --start-left-dir=/data
