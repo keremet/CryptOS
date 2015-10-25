1b7ce0347eea7928d796003dbf2b0a85e4e2c995
Andrey Sokolov <keremet@solaris.kirov.ru>
2015-10-25 18:33:37 +0400

    Улучшено подключение zvol

--- build/crzvol/crzvol_mount.sh
+++ build/crzvol/crzvol_mount.sh
@@ -1,3 +1,16 @@
-sudo lofiadm -c aes-256-cbc -a /dev/zvol/rdsk/rpool/d /dev/lofi/1 && \
-sudo zpool import -d /dev/lofi data && \
+#!/bin/sh
+sudo lofiadm -c aes-256-cbc -a /dev/zvol/rdsk/rpool/d /dev/lofi/1
+if [ "$?" != "0" ]
+then echo "Ошибка lofiadm. Нажмите Enter для продолжения"
+ read
+ exit 1
+fi
+
+sudo zpool import -d /dev/lofi data
+if [ "$?" != "0" ]
+then echo "Ошибка zpool. Нажмите Enter для продолжения"
+ read
+ exit 1
+fi
+
 gnome-commander --start-left-dir=/data
